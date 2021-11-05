3bb18f7e0de63af91782e0bc38ffc1d8e6a8e851
Ming <i414307@fhict.nl>
2021-11-05 15:49:10 +0100

    Setup CreateSong

--- music4u/src/components/Song/CreateSong.js
+++ music4u/src/components/Song/CreateSong.js
@@ -2,45 +2,39 @@ import React from "react";
 import axios from "axios";
 const baseURL = "http://localhost:8080/";
 
-function CreatSong(){
-    const [post, setPost] = React.useState(null);
-    const [title, setTitle] = React.useState("");
-    const [creatorId, setCreatorId] = React.useState(0);
-    const [file, setFile] = React.useState("");
+function CreateSong(){
+    const [songtitle, setTitle] = React.useState("");
+    const [songcreatorId, setCreatorId] = React.useState(1);
+    const [songfile, setFile] = React.useState("");
 
-    React.useEffect(() => {
-        axios.get(baseURL+"song").then((response) => {
-        setPost(response.data);
-        });
-    }, []);
-
-    if (!post) return null;
-    
     function createPost() {
-        axios.post(baseURL+"song", {
-          })
-          .then((response) => {
-            setPost(response.data);
-          });
+        const song = {title: songtitle, creatorId: songcreatorId, file: songfile};
+
+        axios.post(baseURL+"song/test", song)
+        .catch(error => {
+            this.setState({ errorMessage: error.message });
+            console.error('There was an error!', error);
+        });
     }
+
     
     return(
         <form onSubmit={createPost}>
             <label>
-            Title:
-            <input type="text" value={title} onChange={(e) => setTitle(e.target.value)} />
+            Title (Must start with a capital lette):
+            <input type="text" value={songtitle} onChange={(e) => setTitle(e.target.value)} />
             </label>
             <label>
             CreatorId:
-            <input type="text" value={creatorId} onChange={(e) => setCreatorId(e.target.value)} />
+            <input type="text" value={songcreatorId} onChange={(e) => setCreatorId(e.target.value)} />
             </label>
             <label>
             File:
-            <input type="text" value={file} onChange={(e) => setFile(e.target.value)} />
+            <input type="text" value={songfile} onChange={(e) => setFile(e.target.value)} />
             </label>
             <input type="submit" value="Submit" />
         </form>
     )
 }
 
-export default CreatSong;
+export default CreateSong;
